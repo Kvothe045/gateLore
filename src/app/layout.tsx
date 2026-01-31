8307f5f3bcef7cef13342bb2da37ad390e911dd8
Kvothe045 <aryansrivastav106@gmail.com>
2026-02-01 02:19:51 +0530

    loca

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: 'Lores',
-  description: 'Study Den',
+export const metadata: Metadata = {
+  title: "Gate Lores",
+  description: "Restricted Access",
   robots: {
     index: false,
     follow: false,
-    nocache: true,
     googleBot: {
       index: false,
       follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
     },
   },
 };
@@ -35,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased bg-[#050505] text-slate-200">
         {children}
       </body>
     </html>
